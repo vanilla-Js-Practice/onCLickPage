37d4c56e78fbaf5e24f5da0243738ba9fc58dbce
kkmali <84914086+KK-tech-byte1991@users.noreply.github.com>
2023-08-24 20:56:16 +0530

    Added more buttons

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 document.getElementById("button").addEventListener("click", function () {
   alert("Hi it's Vanilla");
 });
+
+document.getElementById("app").innerHTML = `
+<h1>Hi Its Vanilla Js</h1>
+<p>Ready To Learn and Explore the fullest!!</p>
+`;
+
+document.getElementById("print").addEventListener("click", function () {
+  document.getElementById(
+    "name"
+  ).innerHTML = `Hi Its Kishor the Marvel and Maverick`;
+});
+
+document.getElementById("erase").addEventListener("click", function () {
+  document.getElementById("name").innerHTML = ``;
+});
